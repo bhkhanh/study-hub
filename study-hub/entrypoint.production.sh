01dd5e0ba9349a284552420f930c9c1fcf5ebb14
bhkhanh <bhkhanh@hotmail.com>
2025-10-31 14:04:34 +0700

    add option --workers

--- study-hub/entrypoint.production.sh
+++ study-hub/entrypoint.production.sh
@@ -6,4 +6,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting web server with Gunicorn..."
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
